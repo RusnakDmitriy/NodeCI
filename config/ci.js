34788cc3db14795f75e80d0041ca4cc903ce2e7d
RusnakDmitriy <rusnak179@gmail.com>
2020-05-04 21:52:07 +0300

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -4,8 +4,7 @@ module.exports = {
     '566943383582-lq6sl4nem3u57csrh804g9cm50spbhs0.apps.googleusercontent.com',
   googleClientSecret:
     /*'KnH-rZC23z4fr2CN4ISK4srN',*/ '8xvgYwLO_WK7YM30_UyiF3Oy',
-  mongoURI:
-    'mongodb://DmitriyRusnak:Rabota19121980@cluster0-shard-00-00-1hoec.mongodb.net:27017,cluster0-shard-00-01-1hoec.mongodb.net:27017,cluster0-shard-00-02-1hoec.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
 };
